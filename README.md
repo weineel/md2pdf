25c7e2b006da99f5e65126d20d5856f960c4f1af
weineel <lijufeng@patsnap.com>
2023-12-29 19:20:47 +0800

    feat: 重构，把 转换和连接拆分成两个子命令，可以通过 || 的方式连接两个命令实现更多能力

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # md2pdf
 
 markdown 转 PDF，支持把多个 markdown 文件转成 PDF 并合并到一个 PDF 文件。
+
+## TODO
+
+* [ ] 使用 GhostScript 压缩 pdf --no-compression 不压缩（单文件也要实现）, [参考](https://dev.to/woovi/how-to-reduce-the-file-size-of-a-pdf-using-nodejs-50b2)，如果质量不满意，可以使用 PDF Squeezer for Mac 应用手动压缩效果更好。
+* [ ] 检查目标文件是否存在，存在则跳过 --no-skip-exist
+* [ ] concat 子命令，连接 pdf 文件
+* [*] 重构，把 转换和连接拆分成两个子命令，可以通过 || 的方式连接两个命令实现更多能力
+* [ ] md 转 pdf 的时候增加随机延时
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { mdToPdf } = require('md-to-pdf');
-const fs = require("fs/promises");
+const pfs = require("fs/promises");
+const fs = require("fs");
 const { PDFDocument } = require("pdf-lib");
 const { program } = require('commander');
 const pkg = require('../package.json');
@@ -44,23 +45,73 @@ async function singleMdToPdf(markdownPath) {
   }
 }
 
-async function convertAll(filenames, { strategy = 'join', sort = true, sortSplitKey = SORT_SPLIT_KEY }) {
-  const outputFilename = process.cwd().split(path.sep).pop();
-  const outputPath = path.join(process.cwd(), `${outputFilename}.pdf`);
-  // 获取当前目录下的所有 md 文件
-  const markdownFiles = await fs.readdir(process.cwd());
+/** 当前目录的目录名，如果转换当前目录下的所有文件将作为目录名 */
+function getCurrentDirname() {
+  return getPathLastName(process.cwd());
+}
+
+function getPathLastName(p) {
+  return p.split(path.sep).pop();
+}
+
+/**
+ * 如果没有指定文件名的话，获取指定目录下的所有 md 文件,
+ */
+async function getDirFiles(dir, { filenames = [], ext = ".md" }) {
+  const markdownFiles = await pfs.readdir(dir);
   // 过滤出 md 文件
   let markdownFilenames = filenames
 
   if (!markdownFilenames?.length) {
     markdownFilenames = markdownFiles.filter((file) =>
-      file.endsWith(".md")
+      file.endsWith(ext)
     );
     if (!markdownFilenames.length) {
-      console.log("当前目录下没有 md 文件");
-      return;
+      throw new Error(`目录 [${dir}] 下没有 ${ext} 文件`)
     }
   }
+  return markdownFilenames;
+}
+
+async function convertAll(
+  filenames,
+  {
+    srcDir,
+    desDir,
+    skipExist = true,
+  }
+) {
+  // 获取当前目录下的所有 md 文件, 或者直接使用指定文件
+  const markdownFilenames = await getDirFiles(srcDir, { filenames });
+  if (!fs.existsSync(desDir)) {
+    // 创建子目录
+    await pfs.mkdir(desDir, { recursive: true });
+  }
+
+  for (let index = 0; index < markdownFilenames.length; index++) {
+    const currentFilename = markdownFilenames[index];
+    const outputDir = path.join(desDir, `${getFilenameWithoutExt(currentFilename)}.pdf`);
+    // 如果目标目录已经存在文件则跳过
+    if (skipExist && fs.existsSync(outputDir)) {
+      continue;
+    }
+    console.log(`正在转换 ${currentFilename}...`)
+    const markdownPath = path.join(srcDir, currentFilename);
+    const pdf = await singleMdToPdf(markdownPath)
+    await pfs.writeFile(outputDir, pdf.content);
+  }
+}
+
+async function concatAll(
+  dir,
+  {
+    output,
+    sort = true,
+    sortSplitKey = SORT_SPLIT_KEY,
+  },
+) {
+  // 过滤出 pdf 文件
+  let markdownFilenames = await getDirFiles(dir, { ext: ".pdf" });
 
   if (sort) {
     // 排序
@@ -71,71 +122,80 @@ async function convertAll(filenames, { strategy = 'join', sort = true, sortSplit
     });
   }
 
-  if (strategy.includes("sep")) {
-    // 创建子目录
-    const pdfsDir = path.join(process.cwd(), outputFilename);
-    await fs.mkdir(pdfsDir, { recursive: true });
-  }
-
   const pdfDoc = await PDFDocument.create();
 
   for (let index = 0; index < markdownFilenames.length; index++) {
     const currentFilename = markdownFilenames[index];
-    console.log(`正在转换 ${currentFilename}...`)
-    const markdownPath = path.join(process.cwd(), currentFilename);
-    const pdf = await singleMdToPdf(markdownPath)
-    if (strategy.includes("sep")) {
-      await fs.writeFile(path.join(process.cwd(), outputFilename, `${getFilenameWithoutExt(currentFilename)}.pdf`), pdf.content);
-    }
-
-    if (strategy.includes("join")) {
-      const currentPdf = await PDFDocument.load(pdf.content);
-      for (let index = 0; index < currentPdf.getPageCount(); index++) {
-        // 合并 pdf
-        const [page] = await pdfDoc.copyPages(currentPdf, [index]);
-        pdfDoc.addPage(page);
-      }
+    console.log(`正在拼接 ${currentFilename}...`)
+    // 加载 pdf
+    const pdfBytes = await pfs.readFile(path.join(dir, currentFilename));
+    const currentPdf = await PDFDocument.load(pdfBytes);
+    for (let index = 0; index < currentPdf.getPageCount(); index++) {
+      // 合并 pdf
+      const [page] = await pdfDoc.copyPages(currentPdf, [index]);
+      pdfDoc.addPage(page);
     }
   }
-
-  if (strategy.includes("join")) {
-    const pdfBytes = await pdfDoc.save({ useObjectStreams: true });
-    await fs.writeFile(outputPath, pdfBytes);
-  }
+  console.log(`正在保存合并后的文件: ${getPathLastName(output)}...`)
+  const pdfBytes = await pdfDoc.save({ useObjectStreams: true });
+  await pfs.writeFile(output, pdfBytes);
 }
 
-async function convertSingle(filename) {
-  const markdownPath = path.join(process.cwd(), filename);
-  const pdf = await singleMdToPdf(markdownPath)
-  const outputPath = path.join(process.cwd(), `${getFilenameWithoutExt(filename)}.pdf`);
-  await fs.writeFile(outputPath, pdf.content);
+function normalizePathParam(filename) {
+  return String(filename || '').trim()
 }
 
 async function main() {
   program
     .name('wn-md-pdf')
-    .description('Convert markdown to pdf')
-    .arguments('[filename]', '如果传入了文件名，则转换窜入的文件，可以传入多个')
-    .option('-s, --strategy <strategy>', '在转换文件时的策略. join: 仅仅合并为一个文件; sep: 保存转换的所有单个文件; sep-join: 两种都要', 'join')
-    .option('--no-sort', '合并为一个文件时不排序')
-    .option('--sort-split-key <sortSplitKey>', '排序分隔符', SORT_SPLIT_KEY)
+    .description('将 markdown 文件转换为 pdf、合并为一个 pdf 等功能')
     .version(pkg.version);
 
-  program.parse(process.argv);
+  // md-to-pdf
+  program.command('convert')
+    .description('转换 markdown 文件')
+    .arguments('[filenames...]', '如果传入了文件名，则转换窜入的文件，可以传入多个')
+    .option('-s, --src-dir [srcDir]', '指定源目录，默认为当前目录')
+    .option('-d, --des-dir [desDir]', '指定目标目录，默认为当前目录同名的子目录')
+    .option('--no-skip-exist', '不设置这个标识，检查目标文件是否存在，存在则跳过，模拟断点续传')
+    .action(async (filenames, options) => {
+      try {
+        const srcDir = path.resolve(normalizePathParam(options.srcDir) || process.cwd());
+        const desDir = path.resolve(normalizePathParam(options.desDir) || getCurrentDirname());
+        await convertAll(filenames, {
+          ...options,
+          srcDir,
+          desDir,
+        })
+        console.log("转换完成");
+      } catch (error) {
+        console.error("转换失败:", error);
+      }
+    })
 
-  try {
-    // 获取第一个参数 作为文件名
-    const filenames = program.args;
-    const options = program.opts();
-    if (filenames.length === 1) {
-      await convertSingle(filenames[0])
-    } else {
-      await convertAll(filenames, options)
-    }
-    console.log("转换完成");
-  } catch (error) {
-    console.error("转换失败:", error);
-  }
+  // concat 指定目录下的所有 pdf 文件
+  program.command('concat')
+    .description('合并指定目录下的所有 pdf 文件')
+    .arguments('[dir]', '指定目录，默认为当前目录同名的子目录')
+    .option('-o, --output [output]', '输出文件名，默认为当前目录同名的 pdf 文件, 如果指定了目录，则默认为指定目录的同名 pdf 文件')
+    .option('--no-sort', '合并为一个文件时不排序')
+    .option('--sort-split-key <sortSplitKey>', '排序分隔符', SORT_SPLIT_KEY)
+    .action(async (dir, options) => {
+      try {
+        console.log("正在合并...", getCurrentDirname());
+        dir = path.resolve(dir || getCurrentDirname());
+        const output = path.resolve(normalizePathParam(options.output) || `${dir}.pdf`);
+        await concatAll(dir, {
+          ...options,
+          output,
+        });
+        console.log("合并完成");
+      } catch (error) {
+        console.error("合并失败:", error);
+      }
+    })
+
+  program.parse(process.argv);
 }
 
 main();
